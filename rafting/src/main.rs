17a415b82037a51d5c91f5dc97e64916136b12b3
Erik Langille <eriklangille@gmail.com>
2022-08-22 08:40:59 -0400

    Ability to try range of sockets

--- rafting/src/main.rs
+++ rafting/src/main.rs
@@ -1,9 +1,31 @@
+use std::net::{SocketAddr, IpAddr};
+
 use tokio::{net::{TcpListener, TcpStream}, io::AsyncReadExt, io::AsyncWriteExt};
 use bytes::BytesMut;
+use std::str::FromStr;
+
+const ADDRESS : &'static str = "127.0.0.1";
+
+struct Socket {
+    addrs: Vec<SocketAddr>,
+}
+
+impl Socket {
+    fn from_range(ports: Vec<u16>) -> Socket {
+        let addrs : Vec<SocketAddr> = ports.clone().into_iter()
+        .map(|port| SocketAddr::new(IpAddr::from_str(ADDRESS).unwrap(), port)).collect();
+        Socket {addrs: addrs}
+    }
+
+    pub fn to_slice(&self) -> &[SocketAddr] {
+        &self.addrs[..]
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let socket_range = Socket::from_range(vec![3000, 3001, 3002]);
+    let listener = TcpListener::bind(socket_range.to_slice()).await.unwrap();
 
     println!("Listening");
 
