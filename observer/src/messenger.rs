23958a0465e9796662a10e67fa2a171bf1fc175f
Erik Langille <eriklangille@gmail.com>
2022-10-18 07:56:14 -0400

    Non-static messages capability

--- observer/src/messenger.rs
+++ observer/src/messenger.rs
@@ -3,9 +3,9 @@ use std::any::{Any, TypeId};
 use std::sync::Mutex;
 
 
-trait Handler<T>
-where T: Message {
-  fn handle(&self, msg: T);
+trait Handler<M>
+where M: Message {
+  fn handle(&self, msg: M);
 }
 
 trait Message {
@@ -15,7 +15,7 @@ trait Message {
 struct RequestVoteResponse {
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct RequestVoteRequest {
   potato: u16
 }
@@ -39,17 +39,19 @@ struct Network
   observers: Mutex<HashMap<TypeId, Box<dyn Any + Send>>> 
 }
 
+static REQ_VOTE_HANDLER: RequestVoteActor = RequestVoteActor {};
+
 impl Network {
   pub fn new() -> Self {
     Network { observers: Mutex::new(HashMap::new()) }
   }
 
-  pub fn notify_observers<M, T>(&mut self, msg: M)
-  where M: Message + 'static,
-  T: Handler<M> + 'static
+  pub fn notify_observers<T, M>(&mut self, msg: M)
+  where T: Handler<M> + 'static,
+  M: Message
   {
     let observers = self.observers.get_mut().unwrap();
-    if let Some(actor) = observers.get(&TypeId::of::<M>()) {
+    if let Some(actor) = observers.get(&TypeId::of::<T>()) {
       match actor.downcast_ref::<T>() {
         Some(actor) => actor.handle(msg),
         None => panic!("Spaghetti!")
@@ -57,22 +59,19 @@ impl Network {
     }
   }
 
-  pub fn when<T, M>(&mut self, handler: T)
+  pub fn when<T, M>(&mut self, handler: &T)
   where T: Handler<M> + Clone + Send + 'static,
-  M: Message + 'static
+  M: Message,
   {
     let observers = self.observers.get_mut().unwrap();
-    observers.insert(TypeId::of::<M>(), Box::new(handler.clone()));
+    observers.insert(TypeId::of::<T>(), Box::new(handler.clone()));
   }
 }
 
 pub fn test() {
   println!("yup");
   let potato_request = RequestVoteRequest {potato: 1};
-  let handler = RequestVoteActor {};
   let mut network = Network::new();
-  network.when(handler);
-  network.notify_observers::<RequestVoteRequest, RequestVoteActor>(potato_request);
+  network.when(&REQ_VOTE_HANDLER);
+  network.notify_observers::<RequestVoteActor, RequestVoteRequest>(potato_request);
 }
-
-
